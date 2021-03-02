141fdf4a32b04d438873d836fd87d405d3efa0f7
Sianats <a.torres.2018@alumnos.urjc.es>
2021-03-02 11:05:12 +0100

    practicas varias

---
+++ color.js
@@ -0,0 +1,18 @@
+//esto sirve para declarar un color (en este caso) y luego ya si eso lo llamas en un archivo distinto para cuando lo tengas que usar, realmente es lo mismo que practica 2 pero por separado
+
+function iniciar() {
+
+    var cuadrado= document.getElementById("mycanvas");
+ 
+    var gl = cuadrado.getContext ("webgl");
+
+    if (!gl) {
+            console.log ("algo esta fallando");
+            return;
+    }
+ 
+    
+    gl.clearColor(1.0, 0.5, 0.25, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
